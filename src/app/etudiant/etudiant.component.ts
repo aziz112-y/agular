193302b8ceea209deb3e4829ada19e873c4895cc
aziz112-y <62853997+aziz112-y@users.noreply.github.com>
2023-10-03 21:35:16 +0100

    Add files via upload

---
+++ src/app/etudiant/etudiant.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Etudiant} from './etudiant' ;
+import {Etudiant1} from './etudiant' ;
+import {Etudiants} from './liste-etudiants'
+@Component({
+  selector: 'app-etudiant',
+  templateUrl: './etudiant.component.html',
+  styleUrls: ['./etudiant.component.css']
+})
+export class EtudiantComponent implements OnInit {
+etudiants_l = Etudiants;
+selectedEtudiant: Etudiant = {} as Etudiant;
+
+constructor(){}
+  ngOnInit(): void{
+
+  }
+OnSelect(etudiant:Etudiant):void{
+  this.selectedEtudiant=etudiant;
+}
+  etudiant1: Etudiant1={
+    id:1,
+    name:'Ali',
+    lastname:'ben Saleh',
+    date:new Date("11-09-2003"),
+    average:16
+  }
+  }
+
+
---
+++ src/app/etudiant/etudiant.ts
@@ -0,0 +1,11 @@
+export interface Etudiant1{
+  id:number;
+  name:string;
+ lastname:string;
+  date:Date;
+  average:number;
+}
+export interface Etudiant{
+  id:number;
+  name:string;
+}
